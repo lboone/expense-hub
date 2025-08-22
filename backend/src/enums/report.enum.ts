6f811909fc51764139f1814f4141c0d04de2139e
Lloyd Boone <lboone@progressive.com>
2025-08-22 16:25:41 -0400

    feat: add report status and frequency enums, and create report and report setting models

---
+++ backend/src/enums/report.enum.ts
@@ -0,0 +1,13 @@
+export enum ReportStatusEnum {
+  SENT = "SENT",
+  PENDING = "PENDING",
+  FAILED = "FAILED",
+}
+
+export enum ReportFrequencyEnum {
+  DAILY = "DAILY",
+  WEEKLY = "WEEKLY",
+  MONTHLY = "MONTHLY",
+  QUARTERLY = "QUARTERLY",
+  ANNUALLY = "ANNUALLY",
+}
